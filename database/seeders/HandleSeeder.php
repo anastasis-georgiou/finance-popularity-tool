5ce0e5d57ca19842a87a59c379e74b1588843d40
366-anastasis <>
2024-12-19 19:57:53 +0200

    update

--- database/seeders/HandleSeeder.php
+++ database/seeders/HandleSeeder.php
@@ -16,6 +16,15 @@ class HandleSeeder extends Seeder
             ['handle' => '@finance_news', 'crawling_freq' => 3600],
             ['handle' => '@crypto_updates', 'crawling_freq' => 1800],
             ['handle' => '@forex_daily', 'crawling_freq' => 7200],
+            ['handle' => '@finance_trader', 'crawling_freq' => 3600],
+            ['handle' => '@all_the_updates', 'crawling_freq' => 1800],
+            ['handle' => '@crypto_daily', 'crawling_freq' => 7200],
+            ['handle' => '@finance_expert', 'crawling_freq' => 3600],
+            ['handle' => '@crypto_mogul', 'crawling_freq' => 1800],
+            ['handle' => '@forex_weekly', 'crawling_freq' => 7200],
+            ['handle' => '@forex_news', 'crawling_freq' => 3600],
+            ['handle' => '@crypto_assets', 'crawling_freq' => 1800],
+            ['handle' => '@daily_trader', 'crawling_freq' => 7200],
         ];
 
         foreach ($handles as $handle) {
